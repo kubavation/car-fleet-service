884e19072c3a3b2ad38bf596134537ffe55bc42d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-20 17:31:03 +0200

    init

---
+++ src/main/java/com/durys/jakub/carfleet/CarFleetApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.carfleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarFleetApplication.class, args);
+	}
+
+}
