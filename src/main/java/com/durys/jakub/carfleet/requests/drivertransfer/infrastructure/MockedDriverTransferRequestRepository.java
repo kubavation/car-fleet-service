f1c07138cdb04bdc372887a9a9397d32e112e06e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-21 20:51:33 +0200

    mocked repo + config

---
+++ src/main/java/com/durys/jakub/carfleet/requests/drivertransfer/infrastructure/MockedDriverTransferRequestRepository.java
@@ -0,0 +1,25 @@
+package com.durys.jakub.carfleet.requests.drivertransfer.infrastructure;
+
+import com.durys.jakub.carfleet.requests.RequestId;
+import com.durys.jakub.carfleet.requests.drivertransfer.domain.DriverTransferRequest;
+import com.durys.jakub.carfleet.requests.drivertransfer.domain.DriverTransferRequestRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MockedDriverTransferRequestRepository implements DriverTransferRequestRepository {
+
+    private static final Map<RequestId, DriverTransferRequest> DB = new HashMap<>();
+
+    @Override
+    public Optional<DriverTransferRequest> load(RequestId requestId) {
+        return Optional.ofNullable(DB.get(requestId));
+    }
+
+    @Override
+    public DriverTransferRequest save(DriverTransferRequest request) {
+        DB.put(request.getRequestId(), request);
+        return load(request.getRequestId()).get();
+    }
+}
