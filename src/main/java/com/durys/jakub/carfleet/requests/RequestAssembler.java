370d556d22326770e3d98780299d177b66fc9d91
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-21 13:28:38 +0200

    inital assembler

---
+++ src/main/java/com/durys/jakub/carfleet/requests/RequestAssembler.java
@@ -0,0 +1,38 @@
+package com.durys.jakub.carfleet.requests;
+
+import com.durys.jakub.carfleet.requests.state.StateBuilder;
+import com.durys.jakub.carfleet.requests.state.StateConfig;
+import com.durys.jakub.carfleet.requests.state.predicates.DriverNotEmptyVerifier;
+import com.durys.jakub.carfleet.requests.state.predicates.RequestContentValidVerifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RequestAssembler {
+
+    private static final String NEW = "NEW";
+    private static final String EDITED = "EDITED";
+    private static final String ACCEPTED = "ACCEPTED";
+    private static final String CANCELLED = "CANCELLED";
+    private static final String REJECTED = "REJECTED";
+
+
+    public StateConfig assemble() {
+        StateBuilder builder = new StateBuilder();
+        builder
+                .beginWith(NEW)
+                .check(new RequestContentValidVerifier());
+
+        builder.from(NEW)
+                .whenContentChanged().to(EDITED);
+        builder.from(EDITED).to(EDITED);
+        builder.from(ACCEPTED).to(CANCELLED);
+        builder.from(NEW)
+                .check(new DriverNotEmptyVerifier())
+                .to(ACCEPTED)
+                .action(null);
+        builder.from(NEW)
+                .to(REJECTED);
+
+        return builder;
+    }
+}
