b42653a64aca2ed7545bbe66ef307d7157fd2e9e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-25 20:59:17 +0200

    addiitional validation method on technical inspection

--- src/main/java/com/durys/jakub/carfleet/cars/availability/DefaultCarAvailabilityService.java
+++ src/main/java/com/durys/jakub/carfleet/cars/availability/DefaultCarAvailabilityService.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.carfleet.cars.availability;
 
+import com.durys.jakub.carfleet.cars.domain.Car;
 import com.durys.jakub.carfleet.cars.domain.CarId;
+import com.durys.jakub.carfleet.cars.domain.CarsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,12 @@ import java.time.LocalDateTime;
 class DefaultCarAvailabilityService implements CarAvailabilityService {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final CarsRepository carsRepository;
 
     @Override
     public boolean available(CarId carId, LocalDateTime from, LocalDateTime to) {
 
-        return !carInExistingTransfer(carId, from, to) && !needsCarOverview(carId, from, to);
+        return !carInExistingTransfer(carId, from, to) && !needsTechnicalInspection(carId, from, to);
 
     }
 
@@ -26,8 +29,15 @@ class DefaultCarAvailabilityService implements CarAvailabilityService {
         return false;
     }
 
-    private boolean needsCarOverview(CarId carId, LocalDateTime from, LocalDateTime to) {
-        //todo
-        return false;
+    private boolean needsTechnicalInspection(CarId carId, LocalDateTime from, LocalDateTime to) {
+
+        Car car = carsRepository.load(carId)
+                .orElseThrow(RuntimeException::new);
+
+        if (to.toLocalDate().isAfter(car.nextTechnicalInspectionAt())) {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/main/java/com/durys/jakub/carfleet/cars/domain/Car.java
+++ src/main/java/com/durys/jakub/carfleet/cars/domain/Car.java
@@ -3,6 +3,8 @@ package com.durys.jakub.carfleet.cars.domain;
 import com.durys.jakub.carfleet.cars.domain.basicinformation.CarBasicInformation;
 import com.durys.jakub.carfleet.cars.domain.tenchicalinspection.TechnicalInspection;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Set;
 
 public class Car {
@@ -20,5 +22,13 @@ public class Car {
         this.carType = carType;
         this.basicInformation = basicInformation;
         this.technicalInspections = technicalInspections;
+    }
+
+
+    public LocalDate nextTechnicalInspectionAt() {
+        return technicalInspections.stream()
+                .max(Comparator.comparing(TechnicalInspection::at))
+                .map(TechnicalInspection::nextAt)
+                .orElse(null);
     }
 }
