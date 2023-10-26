aca874fdf0422967a29a9506f7c915666c8622f7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-26 19:46:39 +0200

    registration number-  model refactor - validation

--- src/main/java/com/durys/jakub/carfleet/cars/domain/basicinformation/RegistrationNumber.java
+++ src/main/java/com/durys/jakub/carfleet/cars/domain/basicinformation/RegistrationNumber.java
@@ -1,3 +1,30 @@
 package com.durys.jakub.carfleet.cars.domain.basicinformation;
 
-public record RegistrationNumber(String value) {}
+import com.durys.jakub.carfleet.common.errors.ValidationError;
+import com.durys.jakub.carfleet.common.errors.ValidationErrorHandler;
+import com.durys.jakub.carfleet.common.errors.ValidationErrorHandlers;
+
+import java.util.Objects;
+
+public class RegistrationNumber {
+
+    private final String value;
+
+    public RegistrationNumber(String value) {
+        this.value = value;
+        test(value, ValidationErrorHandlers.throwingValidationErrorHandler());
+    }
+
+    static void test(String number, ValidationErrorHandler handler) {
+
+        if (Objects.isNull(number)) {
+            handler.handle(new ValidationError("Registration number cannot be empty"));
+        }
+
+        //todo additional validation
+    }
+
+    public String value() {
+        return value;
+    }
+}
