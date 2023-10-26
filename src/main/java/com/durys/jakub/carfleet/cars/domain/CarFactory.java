7348b975754f5658d410e5a58839623cd185b06a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-26 20:58:22 +0200

    carFactory #2

--- src/main/java/com/durys/jakub/carfleet/cars/domain/CarFactory.java
+++ src/main/java/com/durys/jakub/carfleet/cars/domain/CarFactory.java
@@ -7,6 +7,8 @@ import com.durys.jakub.carfleet.common.errors.ValidationErrorHandlers;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CarFactory {
 
@@ -15,13 +17,24 @@ public class CarFactory {
     }
 
 
-    public static class CarBuilder implements BasicInformationFactory {
+    public static class CarBuilder implements InitialFactory, BasicInformationFactory, FinalStepFactory {
 
         private final ValidationErrorHandler errorHandler;
+
+        private CarId carId;
+        private CarType carType;
         private CarBasicInformation carBasicInformation;
+
 
         public CarBuilder(ValidationErrorHandler errorHandler) {
             this.errorHandler = errorHandler;
+        }
+
+        @Override
+        public BasicInformationFactory with(CarId carId, CarType carType) {
+            this.carId = carId;
+            this.carType = carType; //todo validation
+            return this;
         }
 
         @Override
@@ -29,14 +42,24 @@ public class CarFactory {
             this.carBasicInformation = new CarBasicInformation(registrationNumber, vin, fuelType, errorHandler);
             return this;
         }
-    }
 
-    public Car construct() {
-        return
+        @Override
+        public Car construct() {
+            return new Car(carId, carType, carBasicInformation, new HashSet<>()); //
+        }
+
     }
 
     interface BasicInformationFactory {
         CarBuilder withBasicInformation(String registrationNumber, String vin, FuelType fuelType);
+    }
+
+    interface InitialFactory {
+        BasicInformationFactory with(CarId carId, CarType carType);
+    }
+
+    interface FinalStepFactory {
+        Car construct();
     }
 
 }
