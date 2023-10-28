95d3069e4dce97cd5dc0cb2a65e95d7a0ee40ed7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-28 09:12:13 +0200

    validation on mileage vo

--- src/main/java/com/durys/jakub/carfleet/cars/domain/tenchicalinspection/Mileage.java
+++ src/main/java/com/durys/jakub/carfleet/cars/domain/tenchicalinspection/Mileage.java
@@ -1,5 +1,36 @@
 package com.durys.jakub.carfleet.cars.domain.tenchicalinspection;
 
-import java.math.BigDecimal;
+import com.durys.jakub.carfleet.common.errors.ValidationError;
+import com.durys.jakub.carfleet.common.errors.ValidationErrorHandler;
+import com.durys.jakub.carfleet.common.errors.ValidationErrorHandlers;
 
-public record Mileage(BigDecimal value) {}
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Mileage {
+
+    private final BigDecimal value;
+
+    public Mileage(BigDecimal value) {
+        this(value, ValidationErrorHandlers.throwingValidationErrorHandler());
+    }
+
+    public Mileage(BigDecimal value, ValidationErrorHandler errorHandler) {
+        this.value = value;
+        test(value, errorHandler);
+    }
+
+    static void test(BigDecimal value, ValidationErrorHandler handler) {
+
+        if (Objects.isNull(value)) {
+            handler.handle(new ValidationError("Registration number cannot be empty"));
+        }
+
+        //todo additional validation
+    }
+
+    public BigDecimal value() {
+        return value;
+    }
+
+}
