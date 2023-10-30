a4a18929f79131ed8d5bac416fdf52d6ae27602e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-30 19:17:48 +0100

    additional tests

--- src/test/java/com/durys/jakub/carfleet/transfers/domain/TransferPathTest.java
+++ src/test/java/com/durys/jakub/carfleet/transfers/domain/TransferPathTest.java
@@ -47,4 +47,56 @@ class TransferPathTest {
         assertEquals(2, transferPath.getStops().iterator().next().participants().size());
     }
 
+    @Test
+    void shouldRemoveTransferParticipant() {
+
+        ParticipantId participantId = new ParticipantId(UUID.randomUUID());
+        String place = "Krakow";
+        RequestId requestId = new RequestId(UUID.randomUUID());
+
+        TransferPath transferPath = new TransferPath("Warsaw", new HashSet<>(
+                Arrays.asList(new TransferStop("Krakow",
+                                new HashSet<>(
+                                        Arrays.asList(
+                                                new TransferParticipant(
+                                                    new ParticipantId(UUID.randomUUID()),
+                                                    new RequestId(UUID.randomUUID())),
+                                                new TransferParticipant(
+                                                    participantId, requestId)
+
+                                        )
+                                )
+                        )
+                )));
+
+        transferPath.removeParticipant(participantId, place, requestId);
+
+        assertEquals(1, transferPath.getStops().size());
+        assertEquals(1, transferPath.getStops().iterator().next().participants().size());
+    }
+
+    @Test
+    void shouldRemoveTransferParticipantAndTransferStopWhenThereIsNoParticipants() {
+
+        ParticipantId participantId = new ParticipantId(UUID.randomUUID());
+        String place = "Krakow";
+        RequestId requestId = new RequestId(UUID.randomUUID());
+
+        TransferPath transferPath = new TransferPath("Warsaw", new HashSet<>(
+                Arrays.asList(new TransferStop("Krakow",
+                                new HashSet<>(
+                                        Arrays.asList(
+                                                new TransferParticipant(
+                                                        participantId, requestId)
+
+                                        )
+                                )
+                        )
+                )));
+
+        transferPath.removeParticipant(participantId, place, requestId);
+
+        assertEquals(0, transferPath.getStops().size());
+    }
+
 }
