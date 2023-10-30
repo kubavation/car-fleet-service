1dd8c0124ff7d8b06788c955f81731e424f846ee
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-30 21:19:00 +0100

    TransferRequestSubmitted event + handler

--- src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/events/TransferRequestSubmitted.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/events/TransferRequestSubmitted.java
@@ -1,15 +1,9 @@
-package com.durys.jakub.carfleet.requests.transfer.domain.event;
+package com.durys.jakub.carfleet.requests.transfer.instrastructure.in.events;
 
-import com.durys.jakub.carfleet.cars.domain.CarId;
-import com.durys.jakub.carfleet.events.DomainEvent;
-import com.durys.jakub.carfleet.requests.RequestId;
 import com.durys.jakub.carfleet.requests.RequesterId;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-public record TransferRequestAccepted(UUID id, Instant at, RequestId requestId, RequesterId requesterId,
-                               LocalDateTime from, LocalDateTime to, String placeFrom,
-                               String destination, CarId carId) implements DomainEvent {
+public record TransferRequestSubmitted(RequesterId requesterId, LocalDateTime from, LocalDateTime to, String placeFrom,
+                                       String destination, String purpose) {
 }
--- src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/events/handler/TransferRequestSubmittedEventHandler.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/events/handler/TransferRequestSubmittedEventHandler.java
@@ -1,2 +1,19 @@
-package com.durys.jakub.carfleet.requests.transfer.instrastructure.in.events.handler;class TransferRequestSubmittedEventHandler {
+package com.durys.jakub.carfleet.requests.transfer.instrastructure.in.events.handler;
+
+import com.durys.jakub.carfleet.requests.transfer.application.TransferRequestService;
+import com.durys.jakub.carfleet.requests.transfer.instrastructure.in.events.TransferRequestSubmitted;
+import com.durys.jakub.carfleet.requests.vo.RequestPurpose;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+
+@RequiredArgsConstructor
+class TransferRequestSubmittedEventHandler {
+
+    private final TransferRequestService transferRequestService;
+
+    @EventListener
+    public void handle(TransferRequestSubmitted event) {
+        transferRequestService.create(event.requesterId(), event.from(), event.to(), new RequestPurpose(event.purpose()),
+                event.placeFrom(), event.destination());
+    }
 }
