c215c9771a286057a3865ddc06e3203155b630b2
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-31 15:22:53 +0100

    rest driver + acl

--- src/main/java/com/durys/jakub/carfleet/drivers/infrastructure/external/provider/RestDriverProvider.java
+++ src/main/java/com/durys/jakub/carfleet/drivers/infrastructure/external/provider/RestDriverProvider.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.carfleet.drivers.infrastructure.external.provider;
 
 import com.durys.jakub.carfleet.drivers.domain.Driver;
+import com.durys.jakub.carfleet.drivers.domain.DriverId;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
@@ -14,6 +15,20 @@ class RestDriverProvider implements DriverProvider {
 
     @Override
     public Flux<Driver> loadAllBy(String link) {
-        return null; //todo
+        return webClient
+                .get()
+                .uri(link)
+                .retrieve()
+                .bodyToFlux(User.class)
+                .map(DriverACL::convert);
+    }
+
+    static class DriverACL {
+        public static Driver convert(User user) {
+            return new Driver(
+                    new DriverId(user.userId().value()),
+                    user.firstName(),
+                    user.lastName());
+        }
     }
 }
