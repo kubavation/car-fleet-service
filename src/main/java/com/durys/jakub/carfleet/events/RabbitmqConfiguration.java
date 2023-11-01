9456a49320058b7c399914eeb73f07b295d72225
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-01 21:54:01 +0100

    intial configuration of messaging

--- src/main/java/com/durys/jakub/carfleet/events/RabbitmqConfiguration.java
+++ src/main/java/com/durys/jakub/carfleet/events/RabbitmqConfiguration.java
@@ -1,9 +1,15 @@
 package com.durys.jakub.carfleet.events;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,5 +29,21 @@ class RabbitmqConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(cachingConnectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    Declarables configuration(@Value("${queue.driver-request-acceptation}") String driverTransferQueue,
+                              @Value("${exchange.driver-request-acceptation}") String driverTransferExchange) {
+
+        Queue driverTransferRequestQueue = new Queue(driverTransferQueue, false);
+        TopicExchange driverTransferRequestExchange = new TopicExchange(driverTransferExchange);
+
+        return new Declarables(
+                driverTransferRequestQueue,
+                driverTransferRequestExchange,
+                BindingBuilder.bind(driverTransferRequestQueue)
+                        .to(driverTransferRequestExchange)
+                        .with("acceptation"));
+
     }
 }
