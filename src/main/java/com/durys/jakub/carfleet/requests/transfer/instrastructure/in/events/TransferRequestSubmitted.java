50108b3bb37d8d98f956e7722c75631172bfbe8d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-04 23:05:09 +0100

    renaming

--- src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/events/TransferRequestSubmitted.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/events/TransferRequestSubmitted.java
@@ -5,6 +5,6 @@ import com.durys.jakub.carfleet.sharedkernel.cars.CarType;
 
 import java.time.LocalDateTime;
 
-public record TransferRequestSubmitted(RequesterId requesterId, LocalDateTime from, LocalDateTime to, String placeFrom,
+public record TransferRequestSubmitted(RequesterId requesterId, LocalDateTime from, LocalDateTime to, String departure,
                                        String destination, String purpose, CarType carType) {
 }
