7f48c4d0d125b605beaad8f737b2effc9ae89310
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-04 23:20:35 +0100

    additional endpoints

--- src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/TransferRequestController.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/TransferRequestController.java
@@ -1,9 +1,14 @@
 package com.durys.jakub.carfleet.requests.transfer.instrastructure.in;
 
 import com.durys.jakub.carfleet.requests.transfer.application.TransferRequestService;
+import com.durys.jakub.carfleet.requests.transfer.domain.TransferRequest;
 import com.durys.jakub.carfleet.requests.transfer.instrastructure.in.model.SubmitTransferRequest;
+import com.durys.jakub.carfleet.sharedkernel.requests.RequestId;
 import com.durys.jakub.carfleet.sharedkernel.requests.RequesterId;
+import com.durys.jakub.carfleet.state.ChangeCommand;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transfer-requests")
@@ -24,6 +29,18 @@ class TransferRequestController {
                 transferRequest.destination(), transferRequest.carType());
     }
 
+    @PatchMapping("/{requestId}")
+    void changeContent(@PathVariable UUID requestId, @RequestBody SubmitTransferRequest transferRequest) {
+
+        transferRequestService.change(new RequestId(requestId), transferRequest.from(), transferRequest.to(),
+                transferRequest.purpose(), transferRequest.departure(),
+                transferRequest.destination(), transferRequest.carType());
+    }
+
+    @PatchMapping("/{requestId}/rejection")
+    void reject(@PathVariable UUID requestId) {
+        transferRequestService.changeStatus(new RequestId(requestId), new ChangeCommand(TransferRequest.Status.REJECTED));
+    }
 
 
 }
