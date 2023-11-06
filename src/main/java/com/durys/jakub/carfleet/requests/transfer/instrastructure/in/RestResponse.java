8e611c141c8779c62823a94ef1f7db798aaa86c4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-06 19:50:58 +0100

    additional fields to restResponse

--- src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/RestResponse.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/RestResponse.java
@@ -1,11 +1,36 @@
 package com.durys.jakub.carfleet.requests.transfer.instrastructure.in;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
+@Getter
 class RestResponse extends RepresentationModel<RestResponse> {
+
+    enum OperationStatus {
+       Success, Failure
+    }
+
     private final UUID resourceId;
+    private final OperationStatus status;
+    private final Set<String> additionalMessages;
+
+    private RestResponse(UUID resourceId, OperationStatus status, Set<Exception> exceptions) {
+        this.resourceId = resourceId;
+        this.status = status;
+        this.additionalMessages = exceptions.stream()
+                .map(Throwable::getMessage)
+                .collect(Collectors.toSet());
+    }
+
+    static RestResponse success(UUID resourceId) {
+        return new RestResponse(resourceId, OperationStatus.Success, Set.of());
+    }
+    
+    static RestResponse failure(UUID resourceId, Set<Exception> exceptions) {
+        return new RestResponse(resourceId, OperationStatus.Failure, exceptions);
+    }
 }
