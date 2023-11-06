c55e221ed1fd4c26f27715efdaf0dbb9f856b408
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-06 20:55:07 +0100

    tests - validation on errorDetails

--- src/test/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/TransferRequestControllerTest.java
+++ src/test/java/com/durys/jakub/carfleet/requests/transfer/instrastructure/in/TransferRequestControllerTest.java
@@ -1,7 +1,9 @@
 package com.durys.jakub.carfleet.requests.transfer.instrastructure.in;
 
+import com.durys.jakub.carfleet.cars.domain.CarId;
 import com.durys.jakub.carfleet.requests.transfer.application.TransferRequestService;
 import com.durys.jakub.carfleet.requests.transfer.domain.TransferRequest;
+import com.durys.jakub.carfleet.requests.transfer.domain.state.commands.AssignTransferCarCommand;
 import com.durys.jakub.carfleet.requests.transfer.instrastructure.in.model.SubmitTransferRequest;
 import com.durys.jakub.carfleet.sharedkernel.cars.CarType;
 import com.durys.jakub.carfleet.sharedkernel.requests.RequestId;
@@ -10,35 +12,31 @@ import com.durys.jakub.carfleet.state.ChangeCommand;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.vavr.control.Either;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import static com.durys.jakub.carfleet.requests.transfer.domain.TransferRequest.Status.CANCELLED;
 import static com.durys.jakub.carfleet.requests.transfer.domain.TransferRequest.Status.REJECTED;
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TransferRequestController.class)
 class TransferRequestControllerTest {
@@ -147,7 +145,7 @@ class TransferRequestControllerTest {
         UUID requestId = UUID.randomUUID();
 
         Mockito.when(transferRequestService.changeStatus(new RequestId(requestId), new ChangeCommand(REJECTED)))
-                .thenReturn(Either.left(Set.of(new RuntimeException("Unexpected Exception"))));
+                .thenReturn(Either.left(List.of(new RuntimeException("Unexpected Exception"))));
 
         mockMvc.perform(MockMvcRequestBuilders
                         .patch("/transfer-requests/%s/rejection".formatted(requestId.toString()))
@@ -165,14 +163,33 @@ class TransferRequestControllerTest {
 
         UUID requestId = UUID.randomUUID();
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+        mockMvc.perform(MockMvcRequestBuilders
                         .patch("/transfer-requests/%s/cancellation".formatted(requestId.toString()))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+                .andExpect(status().isOk());
+    }
 
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    @Test
+    void cancelTransferRequest_shouldReturn200AndErrorDetails() throws Exception {
+
+
+        UUID requestId = UUID.randomUUID();
+
+        Mockito.when(transferRequestService.changeStatus(new RequestId(requestId), new ChangeCommand(CANCELLED)))
+                .thenReturn(Either.left(
+                        List.of(
+                                new RuntimeException("Unexpected Exception 1"),
+                                new RuntimeException("Unexpected Exception 2"))));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/transfer-requests/%s/cancellation".formatted(requestId.toString()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("Failure"))
+                .andExpect(jsonPath("$.additionalMessages")
+                        .value(containsInAnyOrder("Unexpected Exception 1", "Unexpected Exception 2")));
     }
 
     @Test
@@ -181,15 +198,38 @@ class TransferRequestControllerTest {
         UUID requestId = UUID.randomUUID();
         UUID carId = UUID.randomUUID();
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+        mockMvc.perform(MockMvcRequestBuilders
                         .patch("/transfer-requests/%s/acceptation".formatted(requestId.toString()))
                         .param("assignedCarId", carId.toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+                .andExpect(status().isOk());
 
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    @Test
+    void acceptTransferRequest_shouldReturn200AndErrorDetails() throws Exception {
+
+        UUID requestId = UUID.randomUUID();
+        UUID carId = UUID.randomUUID();
+
+        Mockito.when(transferRequestService.changeStatus(new RequestId(requestId), new AssignTransferCarCommand(new CarId(carId))))
+                .thenReturn(Either.left(
+                        List.of(
+                                new RuntimeException("Unexpected Exception 1"),
+                                new RuntimeException("Unexpected Exception 2"))));
+
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/transfer-requests/%s/acceptation".formatted(requestId.toString()))
+                        .param("assignedCarId", carId.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("Failure"))
+                .andExpect(jsonPath("$.additionalMessages")
+                        .value(containsInAnyOrder("Unexpected Exception 1", "Unexpected Exception 2")));
+
     }
 
 
