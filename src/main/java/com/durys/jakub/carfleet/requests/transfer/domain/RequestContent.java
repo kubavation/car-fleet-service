0d7f8425e96cc664ae49022c754e07f6b92da327
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-07 18:33:29 +0100

    fix - requestContent

--- src/main/java/com/durys/jakub/carfleet/requests/transfer/domain/RequestContent.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/domain/RequestContent.java
@@ -1,11 +1,13 @@
 package com.durys.jakub.carfleet.requests.transfer.domain;
 
+import com.durys.jakub.carfleet.common.errors.ValidationError;
+import com.durys.jakub.carfleet.common.errors.ValidationErrorHandler;
+import com.durys.jakub.carfleet.common.errors.ValidationErrorHandlers;
 import com.durys.jakub.carfleet.requests.vo.RequestPurpose;
 import com.durys.jakub.carfleet.sharedkernel.cars.CarType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -21,18 +23,23 @@ class RequestContent {
     @AttributeOverride(name = "value", column = @Column(name = "PURPOSE"))
     private final RequestPurpose purpose;
 
-    private final String departure;
-    private final String destination;
+    @Embedded
+    private final TransferRoute transferRoute;
 
     @Enumerated(EnumType.STRING)
     private final CarType carType;
 
-    RequestContent(LocalDateTime from, LocalDateTime to, RequestPurpose purpose,
+    RequestContent(LocalDateTime from, LocalDateTime to, String purpose,
                    String departure, String destination, CarType carType) {
-        this.period = new Period(from, to);
-        this.purpose = purpose;
-        this.departure = departure;
-        this.destination = destination;
+        this(from, to, purpose, departure, destination, carType, ValidationErrorHandlers.throwingValidationErrorHandler());
+    }
+
+    RequestContent(LocalDateTime from, LocalDateTime to, String purpose,
+                   String departure, String destination, CarType carType, ValidationErrorHandler handler) {
+
+        this.period = new Period(from, to, handler);
+        this.purpose = new RequestPurpose(purpose, handler);
+        this.transferRoute = new TransferRoute(departure, destination, handler);
         this.carType = carType;
     }
 
@@ -49,11 +56,11 @@ class RequestContent {
     }
 
     public String departure() {
-        return departure;
+        return transferRoute.departure();
     }
 
     public String destination() {
-        return destination;
+        return transferRoute.destination();
     }
 
     public CarType carType() {
@@ -61,18 +68,30 @@ class RequestContent {
     }
 
 
+    public static void test(LocalDateTime from, LocalDateTime to, String purpose, String departure,
+                     String destination, CarType carType,
+                     ValidationErrorHandler handler) {
+
+        Period.test(from, to, handler);
+        RequestPurpose.test(purpose, handler);
+        TransferRoute.test(departure, destination, handler);
+
+        if (Objects.isNull(carType)) {
+            handler.handle(new ValidationError("Car type cannot be empty"));
+        }
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RequestContent that = (RequestContent) o;
         return Objects.equals(period, that.period) && Objects.equals(purpose, that.purpose)
-                && Objects.equals(departure, that.departure)
-                && Objects.equals(destination, that.destination) && carType == that.carType;
+                && Objects.equals(transferRoute, that.transferRoute) && carType == that.carType;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(period, purpose, departure, destination, carType);
+        return Objects.hash(period, purpose, transferRoute, carType);
     }
 }
