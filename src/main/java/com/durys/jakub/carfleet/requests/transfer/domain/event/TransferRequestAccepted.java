359163dd5065005b38d3cf778fe1888b35c67980
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-09 20:18:40 +0100

    additional field in TransferRequestAccepted event

--- src/main/java/com/durys/jakub/carfleet/requests/transfer/domain/event/TransferRequestAccepted.java
+++ src/main/java/com/durys/jakub/carfleet/requests/transfer/domain/event/TransferRequestAccepted.java
@@ -2,6 +2,7 @@ package com.durys.jakub.carfleet.requests.transfer.domain.event;
 
 import com.durys.jakub.carfleet.cars.domain.CarId;
 import com.durys.jakub.carfleet.events.DomainEvent;
+import com.durys.jakub.carfleet.sharedkernel.cars.CarType;
 import com.durys.jakub.carfleet.sharedkernel.requests.RequestId;
 import com.durys.jakub.carfleet.sharedkernel.requests.RequesterId;
 import com.durys.jakub.carfleet.requests.transfer.domain.TransferRequest;
@@ -11,11 +12,11 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record TransferRequestAccepted(UUID id, Instant at, RequestId requestId, RequesterId requesterId,
-                               LocalDateTime from, LocalDateTime to, String departure,
-                               String destination, CarId carId) implements DomainEvent {
+                                      LocalDateTime from, LocalDateTime to, String departure,
+                                      String destination, CarId carId, CarType carType) implements DomainEvent {
 
     public TransferRequestAccepted(TransferRequest request) {
         this(UUID.randomUUID(), Instant.now(), request.requestId(), request.requesterId(), request.transferFrom(),
-                request.transferTo(), request.departure(), request.destination(), request.assignedCar());
+                request.transferTo(), request.departure(), request.destination(), request.assignedCar(), request.carType());
     }
 }
