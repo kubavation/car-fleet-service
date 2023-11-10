4c809e32e9318a44850cc181e410526ba934e5ab
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-10 16:17:06 +0100

    transfers - jpa #1

--- src/main/java/com/durys/jakub/carfleet/transfers/domain/TransferId.java
+++ src/main/java/com/durys/jakub/carfleet/transfers/domain/TransferId.java
@@ -1,6 +1,13 @@
 package com.durys.jakub.carfleet.transfers.domain;
 
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.util.UUID;
 
-public record TransferId(UUID value) {
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+public record TransferId(UUID value) implements Serializable {
 }
